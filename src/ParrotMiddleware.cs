27982a188185a88f9eec7de3ccdbadd89558b119
s1d0r <sidorenko.v.n@gmail.com>
2018-04-15 21:50:18 +0300

    implement IMiddleware interface

--- src/ParrotMiddleware.cs
+++ src/ParrotMiddleware.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
 
-namespace src
+namespace parrot
 {
-    public class ParrotMiddleware
+    public class ParrotMiddleware : IMiddleware
     {
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            if (context.Activity.Type is ActivityTypes.Message)
+            {
+                await context.SendActivity("parrot middleware");
+            }
+        }
     }
 }
